0b2226ec6c9a1384ff46c086d746f5e18d73affc
adryan0907 <449235@student.fontys.nl>
2024-01-08 16:06:06 +0100

    Adryan - Survey page layout cards

--- service-worker.js
+++ service-worker.js
@@ -13,4 +13,32 @@ self.addEventListener("install",(installing)=>{
   self.addEventListener("push",(pushing)=>{
       console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
   })
+
+  
+
+   // Get the canvas element
+   var ctx = document.getElementById('pieChart').getContext('2d');
+
+   // Chart data
+   var data = {
+     labels: ['Red', 'Blue', 'Yellow'],
+     datasets: [{
+       data: [30, 40, 30], // Example data percentages
+       backgroundColor: ['red', 'blue', 'yellow']
+     }]
+   };
+ 
+   // Configuration options
+   var options = {
+     responsive: true,
+     maintainAspectRatio: false
+     // Other chart options can be set here
+   };
+ 
+   // Create the pie chart
+   var myPieChart = new Chart(ctx, {
+     type: 'pie',
+     data: data,
+     options: options
+   });
   

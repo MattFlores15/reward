18398077087aca9f78d36223cb8e0a0da9ba1a40
Matias Flores <matias.flores@logichainge.com>
2024-01-08 14:22:49 +0100

    Matias-bottom menu changes//manifest.json route fix (all)

--- js/bottom-menu.js
+++ js/bottom-menu.js
@@ -1,13 +1,27 @@
-// mobile-bottom-menu.js
 document.addEventListener('DOMContentLoaded', function () {
-  const currentPage = window.location.pathname.split('/').pop(); // Get the current page
-
-  // Add 'active' class to the corresponding menu item based on the current page
   const menuItems = document.querySelectorAll('.bottom-buttons a');
+
   menuItems.forEach((item) => {
-    const href = item.getAttribute('href');
-    if (currentPage === href) {
-      item.classList.add('active');
-    }
+    item.addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent the default behavior of the link
+      
+      const href = item.getAttribute('href');
+      navigateToPage(href);
+    });
   });
+
+  function navigateToPage(page) {
+    // Here you can use any logic you need to load the content of the page
+    // For simplicity, let's assume you have a function loadPageContent(url) to load the content
+    loadPageContent(page);
+
+    // Optionally, you can update the URL using history API
+    history.pushState(null, null, page);
+  }
+
+  function loadPageContent(url) {
+    // You can use AJAX or any other method to load the content of the page
+    // For simplicity, let's assume you have a function loadPage(url) to load the content
+    loadPage(url);
+  }
 });

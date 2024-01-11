20f7525389b550077d95d30c17302f9ff2e803c0
robijntje <44802556+robijntje@users.noreply.github.com>
2024-01-11 11:13:32 +0100

    Added surveyjs survey
    Added a survey powered by SurveyJS in survey-view-2.html.

---
+++ js/survey2json.js
@@ -0,0 +1,109 @@
+const json = {
+    "locale": "nl",
+    "title": "Vragenlijst 2",
+    "description": "Kan u de volgende vragen beantwoorden met betrekking tot het online platform van Samen aan Z?",
+    "logoPosition": "right",
+    "completedBeforeHtml": {
+     "nl": "<h3>Je hebt deze vragenlijst al ingevuld</h3>"
+    },
+    "loadingHtml": {
+     "nl": "<h3>Vragenlijst wordt geladen</h3>"
+    },
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question1",
+        "title": "Ik denk dat ik dit systeem vaak zou willen gebruiken.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question2",
+        "title": "Ik vind het systeem onnodig ingewikkeld.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question3",
+        "title": "Ik vind het systeem gemakkelijk te gebruiken.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question4",
+        "title": "Ik denk dat ik de hulp van een expert nodig heb om dit systeem te kunnen gebruiken.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question5",
+        "title": "Ik vind de verschillende functies van dit systeem goed geïntegreerd.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question6",
+        "title": "Ik denk dat er te veel tegenstrijdigheden in dit systeem zitten.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question7",
+        "title": "Ik kan me voorstellen dat de meeste mensen heel snel leren om dit systeem te gebruiken.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question8",
+        "title": "Ik vind het systeem heel lastig om te gebruiken.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question9",
+        "title": "Ik voelde me zelfverzekerd tijdens het gebruik van het systeem.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       },
+       {
+        "type": "rating",
+        "name": "question10",
+        "title": "Ik moest veel dingen leren voordat ik met het systeem aan de slag kon gaan.",
+        "description": "Links = Helemaal niet akkoord / Rechts = Helemaal akkoord",
+        "isRequired": true
+       }
+      ]
+     }
+    ],
+    "cookieName": "vragenlijst2",
+    "showCompletedPage": false,
+    "navigateToUrl": "https://samenaanz.netlify.app/components/surveys/survey-finish",
+    "showProgressBar": "both",
+    "progressBarType": "questions",
+    "pagePrevText": {
+     "nl": "Vorige"
+    },
+    "pageNextText": {
+     "nl": "Volgende"
+    },
+    "completeText": {
+     "nl": "Voltooi"
+    },
+    "previewText": {
+     "nl": "Voorbeeld"
+    },
+    "editText": {
+     "nl": "Aanpassen"
+    }
+   }
